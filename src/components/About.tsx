aa5b8eeb12b3a558a2ad6a5a0d412e1d9c1e8bbb
Alex Avila <alex.avila.09@outlook.com>
2024-10-23 22:16:36 -0600

    add contact links to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,19 @@
+import { IconContext } from "react-icons";
+import { CiMail, CiLinkedin } from "react-icons/ci";
+
+const links = [
+  {
+    label: "alex.avila.09@outlook.com",
+    href: "mailto:alex.avila.09@outlook.com",
+    icon: <CiMail />,
+  },
+  {
+    label: "@alex-avilx",
+    href: "https://www.linkedin.com/in/alex-avilx/",
+    icon: <CiLinkedin />,
+  },
+];
+
 function About() {
   return (
     <div>
@@ -19,6 +35,26 @@ function About() {
           deployment processes, creating pixel-perfect user interfaces and web
           performance optimization. Helping clients bring their visions to life.
         </p>
+
+        <ul className="mt-5 flex flex-col gap-y-3">
+          {links.map((link) => (
+            <li key={link.href} className="flex items-center gap-x-2.5">
+              {/* TODO: add a 'copy' button so visitors can copy the email address instead */}
+              <IconContext.Provider
+                value={{ className: "size-3.5 shrink-0", size: "24" }}
+              >
+                {link.icon}
+              </IconContext.Provider>
+              <a
+                className="text-[13px] text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:decoration-2 focus:outline-none dark:text-neutral-500 dark:hover:text-neutral-400"
+                href={link.href}
+                target="_blank"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
