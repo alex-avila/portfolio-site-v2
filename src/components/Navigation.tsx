07f5db9fd50f91308ec0416a2300651499f2877e
Alex Avila <alex.avila.09@outlook.com>
2024-10-27 23:42:16 -0600

    add radial gradient to the NavigationPill if it's button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -156,19 +156,24 @@ function Navigation({ sections, onToggleTheme }: INavigationProps) {
 }
 
 function NavigationPill({ children, isButton = false }: { children?: React.ReactNode; isButton?: boolean }) {
+  let noiseClasses = "absolute  bg-transparent bg-[size:220px] bg-repeat contrast-150";
+
+  if (!isButton) {
+    noiseClasses = `${noiseClasses} inset-0 size-full opacity-75 dark:opacity-25 [mask-image:linear-gradient(transparent,black)]`;
+  } else {
+    noiseClasses = `${noiseClasses} bottom-0 left-0 size-full opacity-100 dark:opacity-60 [mask-image:radial-gradient(circle_at_100%_10%,rgba(0,0,0,0.15)_55%,black_100%)]`;
+  }
+
   return (
-    <div className="group relative mt-4 flex w-max max-w-2xl items-center justify-between rounded-full border border-garden-content-quiet-2 dark:border-forest-content-quiet-2">
+    <div
+      data-is-button={isButton}
+      className="group relative mt-4 flex w-max max-w-2xl items-center justify-between rounded-full border border-garden-content-quiet-2 dark:border-forest-content-quiet-2"
+    >
       {/* grainy, blurry background */}
       {/* based on: https://ibelick.com/blog/create-grainy-backgrounds-with-css */}
       <div className="absolute inset-0 isolate size-full overflow-hidden rounded-full">
-        <div
-          data-is-button={isButton}
-          className="relative isolate size-full backdrop-blur-sm transition-colors data-[is-button=true]:group-hover:bg-garden-content-quiet-2/50 dark:bg-forest-base/50 data-[is-button=true]:group-hover:dark:bg-forest-content-quiet-2/50"
-        >
-          <div
-            className="absolute inset-0 h-full w-full bg-transparent bg-[size:220px] bg-repeat opacity-75 contrast-[150%] [mask-image:linear-gradient(to_top,black,transparent_2.75rem)] dark:opacity-25 dark:brightness-[100%] dark:[mask-image:linear-gradient(to_top,black,transparent_120%)]"
-            style={{ backgroundImage: `url(${noiseSvg})` }}
-          />
+        <div className="relative isolate size-full backdrop-blur transition-colors group-data-[is-button=true]:group-hover:bg-garden-content-quiet-2/50 dark:bg-forest-base/50 group-data-[is-button=true]:group-hover:dark:bg-forest-content-quiet-2/50">
+          <div className={noiseClasses} style={{ backgroundImage: `url(${noiseSvg})` }} />
         </div>
       </div>
 
